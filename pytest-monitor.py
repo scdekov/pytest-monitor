8385732b9b3a7a464506af77b9412a1524f6fc93
svetlio <svetlio1994@gmail.com>
2018-01-31 22:02:37 +0200

    Improve to run tests only on modified files

--- pytest-monitor.py
+++ pytest-monitor.py
@@ -2,41 +2,59 @@
 import os
 import sys
 import subprocess
+import time
 from datetime import datetime
 
-
-def run_tests():
-    process = subprocess.Popen(['py.test', '-q'], stdout=subprocess.PIPE)
-    return process.communicate()[0], not process.returncode
+from watchdog.observers.polling import PollingObserver
+from watchdog.events import RegexMatchingEventHandler
 
 
-def extract_result_mark(result):
-    # TODO: extract with regex
-    last_line = result.strip().split('\n')[-1]  # something like 1 failed, 1 passed in 0.07 seconds
-    return last_line[:last_line.index('in')]
+class RunTestsHandler(RegexMatchingEventHandler):
+    def __init__(self, project_name, *args, **kwargs):
+        super(RunTestsHandler, self).__init__(*args, **kwargs)
+        self.project_name = project_name
+        self._last_result_id = None
 
+        self.on_any_event()  # initial run
 
-def build_title(success, project_name):
-    return '%s TESTS %s at %s' % (project_name,
-                                  'SUCCESS' if success else 'FAIL',
-                                  str(datetime.now().strftime("%H:%M")))
+    def on_any_event(self, event=None):
+        out, success = self._run_tests()
+        result_id = self._extract_result_mark(out)
 
+        if self._last_result_id != result_id:
+            self._notify(success, out, project_name)
+            self._last_result_id = result_id
 
-def notify(success, output, project_name):
-    subprocess.call(['notify-send', build_title(success, project_name), output])
+    def _run_tests(self):
+        process = subprocess.Popen(['py.test', '-q'], stdout=subprocess.PIPE)
+        return process.communicate()[0], not process.returncode
 
+    def _extract_result_mark(self, result):
+        # TODO: extract with regex
+        last_line = result.strip().split('\n')[-1]  # something like 1 failed, 1 passed in 0.07 seconds
+        return last_line[:last_line.index('in')]
 
-def run(project_name):
-    last_result_id = None
-    while True:
-        out, success = run_tests()
-        result_id = extract_result_mark(out)
+    def _build_title(self, success, project_name):
+        return '%s TESTS %s at %s' % (project_name,
+                                      'SUCCESS' if success else 'FAIL',
+                                      str(datetime.now().strftime("%H:%M")))
 
-        if last_result_id != result_id:
-            notify(success, out, project_name)
-            last_result_id = result_id
+    def _notify(self, success, output, project_name):
+        subprocess.call(['notify-send', self._build_title(success, project_name), output])
 
 
 if __name__ == '__main__':
-    project_name = len(sys.argv) > 1 and sys.argv[1] or os.getcwd()
-    run(project_name)
+    path = os.getcwd()
+    project_name = len(sys.argv) > 1 and sys.argv[1] or path
+    handler = RunTestsHandler(project_name, regexes=['.*\.py$'])
+    observer = PollingObserver(timeout=3)
+    observer.schedule(handler, path, recursive=True)
+    observer.start()
+
+    try:
+        while True:
+            time.sleep(1)
+    except KeyboardInterrupt:
+        observer.stop()
+
+    observer.join()
