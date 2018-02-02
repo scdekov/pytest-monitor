b48d835f695486614addf46c6e4ffabbe7b39662
Svetoslav Dekov <svetlio1994@gmail.com>
2018-02-02 13:35:02 +0200

    Show only last line of tests result in the notification

--- pytest_monitor/pytest_monitor.py
+++ pytest_monitor/pytest_monitor.py
@@ -21,28 +21,29 @@ class RunTestsHandler(RegexMatchingEventHandler):
 
     def on_any_event(self, event=None):
         out, success = self._run_tests()
-        result_id = self._extract_result_mark(out)
+        last_line = out.strip().split('\n')[-1]
+
+        result_id = self._extract_result_mark(last_line)
 
         if self._last_result_id != result_id:
-            self._notify(success, out)
+            self._notify(success, last_line)
             self._last_result_id = result_id
 
     def _run_tests(self):
         process = subprocess.Popen(['py.test', '-q'], stdout=subprocess.PIPE)
         return process.communicate()[0], not process.returncode
 
-    def _extract_result_mark(self, result):
+    def _extract_result_mark(self, last_line):
         # TODO: extract with regex
-        last_line = result.strip().split('\n')[-1]  # something like 1 failed, 1 passed in 0.07 seconds
         return last_line[:last_line.index('in')]
 
-    def _notify(self, success, output):
+    def _notify(self, success, last_line):
         icon = "notification-power-disconnected" if not success else "notification-network-wireless-full"
         title = '%s TESTS %s at %s' % (self.project_name,
                                        'SUCCESS' if success else 'FAIL',
                                        str(datetime.now().strftime("%H:%M")))
 
-        notify2.Notification(title, output, icon).show()
+        notify2.Notification(title, last_line, icon).show()
 
 
 def main():
